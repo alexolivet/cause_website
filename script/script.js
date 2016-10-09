d6c7eeabd1c37ed1cb928b4117fd5ef4b8aab57a
Arvid Waasdorp <a.waasdorp@gmail.com>
2016-10-09 11:03:37 +0200

    Some initial changes to the donate page

--- script/script.js
+++ script/script.js
@@ -3,18 +3,36 @@
 $(document).ready(function(){
 
   // Your code here.
-  $( "img.imagepic" ).mouseover(function() {
+  $('img.imagepic').mouseover(function() {
    
-    this.src = this.src.replace("old", "new");
+    this.src = this.src.replace('old', 'new');
 
-  })
+  });
   
-
   // Your code here.
-  $( "img.imagepic" ).mouseout(function() {
+  $('img.imagepic').mouseout(function() {
 
-    this.src = this.src.replace("new", "old");
+    this.src = this.src.replace('new', 'old');
 
-  })
+  });
+
+
+  $('#ownvalue').click(function() {
+    if ($('#ownvalue').val() === 'Other amount') {
+      $('#ownvalue').val('');
+    }
+  });
+
+
+  $('#ownvalue').focusout(function() {
+    if ($('#ownvalue').val() === '') {
+      $('#ownvalue').val('Other amount');
+    }
+  });
+
+
 
 });
+
+
+//When clink on other amount
